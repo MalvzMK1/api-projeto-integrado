efbcade2ac191eda03763008825b1d5538bd0bb3
Matheus Alves <matheus.adeoliveira11@gmail.com>
2022-09-28 10:34:38 -0300

    changes

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 
 const { getStudentsByCourse, getSubjects, getStudent, getStudents, getStudentsByStatus, getStudentsByConclusionYear, filterStudentsByStatus, filterStudentsByConclusionYear, getConclusionYears } = require('../modules/alunos.js');
 const { getCourses } = require('../modules/cursos');
 
 const app = express()
+
+app.use(express.json()) // body-parser
 
 app.use((request, response, next) => {
     response.header('Access-Control-Allow-Origin', '*');
@@ -16,11 +17,10 @@ app.use((request, response, next) => {
 });
 
 app.get('/.netlify/functions/api/cursos', cors(), async (request, response, next) => {
-    let courses = getCourses()
+    const courses = getCourses()
 
     if (courses) {
-        response.status(200);
-        response.json(courses);
+        response.status(200).json(courses);
     } else {
         response.status(404);
     }
@@ -28,11 +28,10 @@ app.get('/.netlify/functions/api/cursos', cors(), async (request, response, next
 
 // Endpoint para listar todos os alunos
 app.get('/.netlify/functions/api/alunos', cors(), async (request, response, next) => {
-    let studentsList = getStudents();
+    const studentsList = getStudents();
 
     if (studentsList) {
-        response.status(200);
-        response.json(studentsList);
+        response.status(200).json(studentsList);
     } else {
         response.status(404);
     }
@@ -40,9 +39,9 @@ app.get('/.netlify/functions/api/alunos', cors(), async (request, response, next
 
 // Endpoint para listar todos os alunos de um curso
 app.get('/.netlify/functions/api/alunos/curso/?', cors(), async (request, response, next) => {
-    let course = request.query.curso;
-    let status = request.query.status;
-    let conclusionDate = request.query.conclusao;
+    const course = request.query.curso;
+    const status = request.query.status;
+    const conclusionDate = request.query.conclusao;
     
     let studentsList = getStudentsByCourse(course);
     if (status != undefined) {
@@ -53,8 +52,7 @@ app.get('/.netlify/functions/api/alunos/curso/?', cors(), async (request, respon
     }
 
     if (studentsList) {
-        response.status(200);
-        response.json(studentsList);
+        response.status(200).json(studentsList);
     } else {
         response.status(404);
     }
@@ -62,12 +60,11 @@ app.get('/.netlify/functions/api/alunos/curso/?', cors(), async (request, respon
 
 // Endpoint para listar as informacoes de um aluno pelo numero de matricula
 app.get('/.netlify/functions/api/aluno/:matricula', cors(), async (request, response, next) => {
-    let studentEnrollment = request.params.matricula;
-    let studentInfo = getStudent(studentEnrollment);
+    const studentEnrollment = request.params.matricula;
+    const studentInfo = getStudent(studentEnrollment);
 
     if (studentInfo) {
-        response.status(200);
-        response.json(studentInfo);
+        response.status(200).json(studentInfo);
     } else {
         response.status(404);
     }
@@ -75,14 +72,14 @@ app.get('/.netlify/functions/api/aluno/:matricula', cors(), async (request, resp
 
 // Endpoint para listar as disciplinas de um aluno pela matricula
 app.get('/.netlify/functions/api/:matricula/disciplinas', cors(), async (request, response, next) => {
-    let studentEnrollment = request.params.matricula;
-    let studentInfo = getStudent(studentEnrollment);
 
-    let subjects = getSubjects(studentInfo)
+    const studentEnrollment = request.params.matricula;
+    const studentInfo = getStudent(studentEnrollment);
+
+    const subjects = getSubjects(studentInfo)
 
     if (subjects) {
-        response.status(200);
-        response.json(subjects);
+        response.status(200).json(subjects);
     } else {
         response.status(404);
     }
@@ -90,12 +87,12 @@ app.get('/.netlify/functions/api/:matricula/disciplinas', cors(), async (request
 
 // Endpoint para listar alunos a partir de um status
 app.get('/.netlify/functions/api/alunos/status/:status', cors(), async (request, response, next) => {
-    let status = request.params.status;
-    let studentsList = getStudentsByStatus(status);
+    const { status } = req.params;
+    
+    const studentsList = getStudentsByStatus(status);
 
     if (studentsList) {
-        response.status(200);
-        response.json(studentsList);
+        response.status(200).json(studentsList);
     } else {
         response.status(404);
     }
@@ -103,26 +100,24 @@ app.get('/.netlify/functions/api/alunos/status/:status', cors(), async (request,
 
 // Endpoint para listar alunos a partir de um ano de conclusao
 app.get('/.netlify/functions/api/alunos/conclusao/:data', cors(), async (request, response, next) => {
-    let date = request.params.data;
-    let studentsList = getStudentsByConclusionYear(date);
+    const { data } = request.params;
+    
+    const studentsList = getStudentsByConclusionYear(data);
 
     if (studentsList) {
-        response.status(200);
-        response.json(studentsList);
+        response.status(200).json(studentsList);
     } else {
         response.status(404);
     }
 });
 
 app.get('/.netlify/functions/api/conclusao/?', cors(), async (request, response, next) => {
-    let course = request.query.curso;
-    let status = request.query.status;
+    const { course, status } = request.query;
 
-    let conclusionYears = getConclusionYears(course, status);
+    const conclusionYears = getConclusionYears(course, status);
 
     if (conclusionYears) {
-        response.status(200);
-        response.json(conclusionYears);
+        response.status(200).json(conclusionYears);
     } else {
         response.status(500);
     }
